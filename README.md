9c4536d5471961cdef1d165a8ac0f3399ce4a7ed
larry <larry@zebra>
2017-10-28 22:16:52 +0200

    exclusion file

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
-# utils
+Misc sysutils
+=============
 
-gitstatjson.pl -- convert git log statistics into json-data (usable with D3)
+  gitstatjson.pl
+  --------------
+
+  convert git log statistics into json-data (usable with D3)
+
+    Usage: echo -e \"/home/my/gitrepo1\\n/home/my/gitrepo2\" | gitstatjson.pl [-v|-v|-a|-H]
+     -v be verbose (to stderr), -a include deleted/created files, -H hourly statistics
+     --ef=<exclude_file> file with lines to exclude
+
+  Exclude file format:
+
+  --ef=exclude.txt
+
+  exclude.txt:
+
+  myrepo1:
+     dummyfile
+     data/dir/
+  myrepo2:
+     foobar
+
--- gitstatjson.pl
+++ gitstatjson.pl
@@ -5,6 +5,7 @@ use strict;
 use warnings;
 
 my $verbose = 0;
+my $gran = "day"; # granularity: hour, day
 
 # Dont include created files (new code has either no brainpower (full of bugs) behind it, or is inherited (dont double-count))
 # Deleted files can also distort for example in code rearrangements with a net code change in zero
@@ -14,17 +15,48 @@ my $magic = "_Zzz";
 my $git = "git"; # Git command
 my $gopt = "log --reverse --summary --numstat --pretty='$magic %ai %cn;%s'"; # options given to git command
 
+my %exclude = ();
+
 foreach my $arg (@ARGV){
   if($arg eq "-h" or $arg eq "--help"){
-    print "Usage: echo -e \"/home/my/repo1\n/home/my/repo2\" | git.pl [-v|-v|-a]\n";
+    print "Usage: echo -e \"/home/my/gitrepo1\\n/home/my/gitrepo2\" | gitstatjson.pl [-v|-v|-a|-H]\n";
+    print " -v be verbose (to stderr), -a include deleted/created files, -H hourly statistics\n";
+    print " --ef=<exclude_file> file with lines to exclude\n";
     exit(1);
   }elsif($arg eq "-v"){
     $verbose++;
   }elsif($arg eq "-a"){
     $skip_create_delete = 0;
+  }elsif($arg eq "-H"){
+    $gran = "hour";
+  }elsif($arg =~ /--ef=(.*)/){
+    open my $fh, '<', $1 or die "Cant open file [$1]";
+    my $repo = "";
+    while(my $line = <$fh>){ # here is the syntax where we parse the exclusion-file
+      chomp $line;
+      if($line =~ /^\s*(.*):\s*$/){ # a colon marks a repo-name
+        $repo = $1;
+      }else{ # other lines belongs to the current repo-name
+        $line =~ s/^\s*(.*)/$1/; # remove prepending spaces
+        push(@{$exclude{$repo}}, $line);
+      }
+    }
+    close $fh;
   }
 }
-print STDERR "Verbose mode.\n" if($verbose);
+
+print STDERR "Verbose mode.\n" if $verbose;
+
+if($verbose > 1){
+  while(my($reponame, $k) = each %exclude){
+    if($#{$exclude{$reponame}} >= 0){
+      print STDERR "exclude lines in repo $reponame:\n";
+      foreach my $x (@{$exclude{$reponame}}){
+        print STDERR "    --->$x\n";
+      }
+    }
+  }
+}
 
 my %ha = (); # Activity across all repos and users
 my %hr = (); # each repo state hash
@@ -39,6 +71,13 @@ while(my $dir = <STDIN>){
   # read the git log output, and update the repo state hash
   open my $fh, '-|', "$git $gopt" or die "Cant run $git";
   while(my $line = <$fh>){
+    my $f = 0;
+    #print STDERR "Searching for exclusion in repo [$repo]\n";
+    foreach my $l (@{$exclude{$repo}}){
+      #print STDERR "Maybe exclude [$repo] [$l] [$line]\n";
+      if($line =~ /$l/){ $f = 1; last }
+    }
+    next if $f;
     chomp $line;
     update_repo_state($repo, $line);
   }
@@ -97,19 +136,25 @@ create_summary_state();
 #### Subs
 
 sub update_repo_state {
-  my($repo, $line) = @_;
+  my($repo, $line, $ex) = @_;
   my $currc = $hr{$repo}{currc}; # last/current commit
-  print STDERR "> $line\n" if $verbose > 1;
+  print STDERR "$ex> $line\n" if $verbose > 1;
   if($line =~ /^$magic\s+(\d+-\d+-\d+\s+\d+:\d+:\d+)\s+(\+\d+)\s+(.*);(.*)/){
     my $date = $1;
+    if($gran eq "hour"){
+      $date =~ s/(\d+-\d+-\d+\s+\d+):.*/$1/;
+    }else{ # default do daily buckets
+      $date =~ s/(\d+-\d+-\d+)\s+.*/$1/;
+    }
+    print STDERR "date: [$date]\n" if $verbose > 1;
     $hr{$repo}{$date}{zone} = $2;
     $hr{$repo}{$date}{user} = $3;
     $hr{$repo}{$date}{subj} = $4;
     $hr{$repo}{currc} = $date; # pointer to last/current commit; KLUDGY way to store state; would need to separate working and final state
   }elsif($line =~ /\s*(\d+)\s+(\d+)\s+(.*)/){
     my $file = $3;
-    $hr{$repo}{$currc}{file}{$file}{add}  = $1; # added lines
-    $hr{$repo}{$currc}{file}{$file}{del}  = $2; # removed lines
+    $hr{$repo}{$currc}{file}{$file}{add}  += $1; # added lines
+    $hr{$repo}{$currc}{file}{$file}{del}  += $2; # removed lines
   }elsif($line =~ /\s+delete mode \d+\s+(.*)/){
     my $file = $1;
     $hr{$repo}{$currc}{file}{$file}{delete} = 1; # mark file as created
@@ -149,6 +194,7 @@ sub create_summary_state {
         }
         $ha{$date}{add} += $add;
         $ha{$date}{del} += $del;
+        print STDERR "    total add/del: $date $add $del\n" if($verbose);
       }
     }
   }
